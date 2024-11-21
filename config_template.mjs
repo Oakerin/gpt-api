f064b02c859d9e6bb942aa2b52b92acda655ff5e
Oakerin <s.spirkin@gmail.com>
2024-11-21 19:26:36 +0300

    Move requestSetRepeatCount to config.js

--- config_template.mjs
+++ config_template.mjs
@@ -3,6 +3,7 @@ export const config = {
     apiKey: '<your_openapi_apiKey>',
     inputFileName: 'batch_input',
     outputFileName: 'batch_output',
+    requestSetRepeatCount: 2,
     maxRetryCount: 10,
     retryInterval: 30 * 1000, // msec
     gptModel: 'gpt-4o'
--- src/create.js
+++ src/create.js
@@ -3,7 +3,7 @@ import readline from 'readline';
 import { config } from "../config.mjs";
 
 const inputFileName = `./src/inputData/${config.inputFileName}.jsonl`;
-const repeatCount = 2
+const repeatCount = config.requestSetRepeatCount
 const systemMessage = {"role": "system", "content": "In the following, we will present you 12 syllogism puzzles that have 2 premises and a conclusion (the sentence after 'Therefore'). Your task will be to judge whether the conclusion NECESSARLY from its two premises."}
 const userMessages = []
 
@@ -52,6 +52,6 @@ async function main(userMessages) {
         console.log('*****************DONE*******************');
         console.log('****************************************');
         console.log(`Input file: ${inputFileName} was created`);
-        console.log(`Requests count: ${repeatCount}`);
+        console.log(`Requests set count: ${repeatCount}`);
     })
 }
