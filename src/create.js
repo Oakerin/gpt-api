16ddc5e972e1a747af84c8db1db161a15fa608f8
Oakerin <s.spirkin@gmail.com>
2024-11-21 19:50:45 +0300

    Add some docs

--- src/create.js
+++ src/create.js
@@ -3,6 +3,7 @@ import readline from 'readline';
 import { config } from "../config.mjs";
 
 const inputFileName = `./src/inputData/${config.inputFileName}.jsonl`;
+const requestType = config.requestSetType
 const repeatCount = config.requestSetRepeatCount
 const systemMessage = {"role": "system", "content": "In the following, we will present you 12 syllogism puzzles that have 2 premises and a conclusion (the sentence after 'Therefore'). Your task will be to judge whether the conclusion NECESSARLY from its two premises."}
 const userMessages = []
@@ -24,14 +25,19 @@ readInterface.on('close', () => {
 
 async function main(userMessages) {
     let text = ''
+    let requests
 
-    const messagesSet = userMessages.map(userMessage => {
-        return [systemMessage, userMessage]
-    })
-
-    let requests = messagesSet.map((messages, i) => {
-        return {"custom_id": `${i}`, "method": "POST", "url": "/v1/chat/completions", "body": {"model": config.gptModel, "messages": messages, "max_tokens": 100}}
-    })
+    if (requestType === 'MULTIPLE') {
+        requests = userMessages
+            .map(userMessage => {
+                return [systemMessage, userMessage]
+            })
+            .map((messages, i) => {
+                return {"custom_id": `${i}`, "method": "POST", "url": "/v1/chat/completions", "body": {"model": config.gptModel, "messages": messages, "max_tokens": 100}}
+            })
+    } else {
+        requests = [{"custom_id": '1', "method": "POST", "url": "/v1/chat/completions", "body": {"model": config.gptModel, "messages": userMessages, "max_tokens": 100}}]
+    }
 
     for (let i = 0; i < repeatCount; i++) {
         requests
