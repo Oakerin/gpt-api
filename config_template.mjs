8d501558edf8f3926ddb6784d4c1a4d82e6ca49f
Oakerin <s.spirkin@gmail.com>
2024-11-21 19:52:41 +0300

    Add some docs

--- config_template.mjs
+++ config_template.mjs
@@ -3,8 +3,17 @@ export const config = {
     apiKey: '<your_openapi_apiKey>',
     inputFileName: 'batch_input',
     outputFileName: 'batch_output',
-    requestSetType: 'SINGLE', // SINGLE | MULTIPLE
-    requestSetRepeatCount: 2,
+    /**
+     * requestSetType - SINGLE | MULTIPLE
+     * You can choose a request style for the chat gpt:
+     * For SINGLE you will generate a one request with your set of messages
+     * For MULTIPLE you will generate a set of request with one message in each other
+     */
+    requestSetType: 'MULTIPLE',
+    /**
+     * requestSetRepeatCount
+     * This option is about how many times you want your request will be repeated
+     */
     maxRetryCount: 10,
     retryInterval: 30 * 1000, // msec
     gptModel: 'gpt-4o'
